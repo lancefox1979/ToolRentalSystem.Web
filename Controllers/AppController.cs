246a21f5ea4e4046a0b0806b0708d20bd59f9e91
Shannon Fisher <lancefox1979@gmail.com>
2020-02-23 18:28:45 -0800

    Change the data collection retrieved from the database
    This controller now retrieves relevant Tool data using the new
    ToolsModel.cs class.

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ToolRentalSystem.Web.Models;
 using ToolRentalSystem.Web.Models.Database;
+using ToolRentalSystem.Web.ViewModels;
 
 namespace ToolRentalSystem.Web.Controllers
 {
@@ -14,16 +15,48 @@ namespace ToolRentalSystem.Web.Controllers
     {
         private readonly ToolRentalSystemDBContext _context;
 
+        private ToolsModel toolsModel;
+
         public AppController(ToolRentalSystemDBContext context)
         {
             _context = context;
+            toolsModel = new ToolsModel();
         }
 
-        public async Task<IActionResult> Tool()
-        {
-            var tools = await _context.Tool.ToArrayAsync();
+        // public async Task<IActionResult> Tool()
+        // {
+        //     var tools = await _context.Tool.ToArrayAsync();
 
-            return View(tools);
+        //     return View(tools);
+        // }
+
+        public IActionResult Tools()
+        {
+            List<ToolsModel> toolsList = new List<ToolsModel>();
+
+            var query = _context.ToolDetail.Join(
+                _context.ToolClassification,
+                toolDetail => toolDetail.ToolClassificationId,
+                toolClass => toolClass.ToolClassificationId,
+                (toolDetail, toolClass) => new
+                {
+                    Type = toolClass.ToolClassification1,
+                    Brand = toolDetail.ToolBrand,
+                    TradeName = toolDetail.TradeName
+                });
+            
+            foreach (var item in query)
+            {
+                ToolsModel model = new ToolsModel();
+
+                model.Type = item.Type;
+                model.Brand = item.Brand;
+                model.TradeName = item.TradeName;
+
+                toolsList.Add(model);
+            }
+
+            return View(toolsList);
         }
 
         public IActionResult Index()
