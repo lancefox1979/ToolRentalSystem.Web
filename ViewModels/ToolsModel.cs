9829bad5103fecfa2c3127e27f4d34a588e514e1
Shannon Fisher <lancefox1979@gmail.com>
2020-02-23 18:25:37 -0800

    Add ViewModel to support Tool queries

---
+++ ViewModels/ToolsModel.cs
@@ -0,0 +1,12 @@
+
+
+namespace ToolRentalSystem.Web.ViewModels
+{
+    public class ToolsModel
+    {
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public string TradeName { get; set; }
+        
+    }
+}
