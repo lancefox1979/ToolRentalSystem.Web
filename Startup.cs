6d3ae17ac4b6fb90a638ad32a2e54bfe7a9b527a
Shannon Fisher <lancefox1979@gmail.com>
2020-03-17 10:42:34 -0500

    Add method to allow management of admin users
    The ManageRoles method provides a code-side means of managing users who
    require various roles for testing purposes. This commit also requires
    modifications to the Identity service configuration due to the addition
    of Identity Roles.

--- Startup.cs
+++ Startup.cs
@@ -37,8 +37,10 @@ namespace ToolRentalSystem.Web
             
             services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(Configuration.GetConnectionString("ToolRentalSystemDB")));
             services.AddDbContext<ToolRentalSystemDBContext>(options => options.UseMySql(Configuration.GetConnectionString("ToolRentalSystemDB")));
-            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
-
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            
             services.AddMvc().AddJsonOptions(options =>
             {
                 options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
@@ -55,6 +57,7 @@ namespace ToolRentalSystem.Web
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
             }
+
             else
             {
                 app.UseExceptionHandler("/Home/Error");
@@ -64,7 +67,6 @@ namespace ToolRentalSystem.Web
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
             app.UseAuthentication();
 
             app.UseMvc(config =>
@@ -73,6 +75,113 @@ namespace ToolRentalSystem.Web
                 config.MapRoute("EditTool", "{controller}/{action}/{detailid}", new { controller = "App", Action = "EditTool" });
                 config.MapRoute("AddTool", "{controller}/{action}/{toolid}", new { controller = "App", Action = "AddTool" });
             });
+
+            //ManageRoles(app.ApplicationServices).GetAwaiter().GetResult();
+        }
+
+        // Major parts of this method borrowed from the following:
+        // https://www.c-sharpcorner.com/article/role-base-authorization-in-asp-net-core-2-1/
+        private async Task ManageRoles(IServiceProvider serviceProvider)
+        {
+            IdentityResult identityResult;
+            string tempAdminPassword = "admin1";
+            string[] roleNames = { "Admin", "Manager", "Employee", "Customer" };
+
+            using (IServiceScope scope = serviceProvider.CreateScope())
+            {
+                RoleManager<IdentityRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                UserManager<IdentityUser> userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                foreach (var role in roleNames)
+                {
+                    bool exists = await roleManager.RoleExistsAsync(role);
+
+                    if (!exists)
+                    {
+                        identityResult = await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
+                
+                IdentityUser userOrtiz = await userManager.FindByEmailAsync("ortiza@toolrentalsystem.com");
+                IdentityUser userFisher = await userManager.FindByEmailAsync("fishers@toolrentalsystem.com");
+                IdentityUser userGarrett = await userManager.FindByEmailAsync("garretts@toolrentalsystem.com");
+                IdentityUser userBiadgilgn = await userManager.FindByEmailAsync("biadgilgnh@toolrentalsystem.com");
+                IdentityUser userHarper = await userManager.FindByEmailAsync("harperd@toolrentalsystem.com");
+                IdentityUser userCustomer = await userManager.FindByEmailAsync("user.customer@toolrentalsystem.com");
+
+                if (userOrtiz == null)
+                {
+                    userOrtiz = new IdentityUser()
+                    {
+                        UserName = "ortiza@toolrentalsystem.com",
+                        Email = "ortiza@toolrentalsystem.com"
+                    };
+
+                    await userManager.CreateAsync(userOrtiz, tempAdminPassword);
+                }
+
+                if (userFisher == null)
+                {
+                    userFisher = new IdentityUser()
+                    {
+                        UserName = "fishers@toolrentalsystem.com",
+                        Email = "fishers@toolrentalsystem.com"
+                    };
+
+                    await userManager.CreateAsync(userFisher, tempAdminPassword);
+                }
+
+                if (userGarrett == null)
+                {
+                    userGarrett = new IdentityUser()
+                    {
+                        UserName = "garretts@toolrentalsystem.com",
+                        Email = "garretts@toolrentalsystem.com"
+                    };
+
+                    await userManager.CreateAsync(userGarrett, tempAdminPassword);
+                }
+
+                if (userBiadgilgn == null)
+                {
+                    userBiadgilgn = new IdentityUser()
+                    {
+                        UserName = "biadgilgnh@toolrentalsystem.com",
+                        Email = "biadgilgnh@toolrentalsystem.com"
+                    };
+
+                    await userManager.CreateAsync(userBiadgilgn, tempAdminPassword);
+                }
+                
+                if (userHarper == null)
+                {
+                    userHarper = new IdentityUser()
+                    {
+                        UserName = "harperd@toolrentalsystem.com",
+                        Email = "harperd@toolrentalsystem.com"
+                    };
+
+                    await userManager.CreateAsync(userHarper, tempAdminPassword);
+                }
+
+                if (userCustomer == null)
+                {
+                    userCustomer = new IdentityUser()
+                    {
+                        UserName = "user.customer@toolrentalsystem.com",
+                        Email = "user.customer@toolrentalsystem.com"
+                    };
+
+                    await userManager.CreateAsync(userCustomer, tempAdminPassword);
+                }
+
+                await userManager.AddToRoleAsync(userOrtiz, "Admin");
+                await userManager.AddToRoleAsync(userFisher, "Admin");
+                await userManager.AddToRoleAsync(userGarrett, "Admin");
+                await userManager.AddToRoleAsync(userBiadgilgn, "Admin");
+                await userManager.AddToRoleAsync(userHarper, "Admin");
+                await userManager.AddToRoleAsync(userCustomer, "Customer");
+            }
         }
     }
 }
